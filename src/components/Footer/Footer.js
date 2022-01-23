67608550709db58dc375e4af41bd8ee9bc2a8ece
Piyush <gaurpiyush001@gmail.com>
2022-01-23 15:59:41 +0530

    first_commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+91-8750-672312">8750672312</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:gaurpiyush001@gmail.com">
+            gaurpiyush001
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Here to Solve Real World Problems with Advanced Technologies</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/gaurpiyush001">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/piyush-gaur-0749051b0/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.instagram.com/piyushgaur26/">
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
